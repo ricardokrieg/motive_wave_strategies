1453bc83f98c8a21fa3d5862643c4ce663aba82f
Ricardo Franco <ricardo.krieg@gmail.com>
2022-08-09 03:06:15 +0000

    FibonacciStrategy.java edited online with Bitbucket

--- FibonacciStrategy.java
+++ FibonacciStrategy.java
@@ -40,17 +40,7 @@ public class FibonacciStrategy extends Study {
 	GraphicManager graphicManager;
 	OrderManager orderManager;
 	SwingManager swingManager;
-	
-	boolean onWave2 = false;
-	int wave2Index = 0;
-	String currentTrend = null;
-	
-	double retraction = 0;
-	double retraction50 = 0;
-	double retraction618 = 0;
-	boolean validRetraction = false;
-	boolean reachedZone = false;
-	boolean invalidatedZone = false;
+	TrendManager trendManager;
 	
 	boolean checkOrders = false;
 	Order currentOrder = null;
@@ -102,6 +92,7 @@ public class FibonacciStrategy extends Study {
 				getSettings().getPath(TTF_LINE));
 		
 		this.swingManager = new SwingManager(getSettings().getInteger(Inputs.STRENGTH));
+		this.trendManager = new TrendManager();
 	}
 	
 	@Override
@@ -139,162 +130,14 @@ public class FibonacciStrategy extends Study {
 		clearFigures();
 	}
 	
-	public void computeTrend(boolean ltfTrend) {
-		currentTrend = null;
-		
-		SwingPoint lastSwingHigh = null;
-		SwingPoint lastSwingLow = null;
-		SwingPoint highestSwingHigh = null;
-		SwingPoint lowestSwingLow = null;
-		SwingPoint leadingSwingHigh = null;
-		SwingPoint leadingSwingLow = null;
-		
-		for (SwingPoint swing : this.swingManager.swingsLTF) {
-			if (!ltfTrend) {
-				if (!this.swingManager.swingsTTFKeys.contains(swing.getIndex())) continue;
-				
-				onWave2 = false;
-			}
-			
-			if (currentTrend == null) {
-				if (swing.isTop()) {
-					currentTrend = "down";
-				} else {
-					currentTrend = "up";
-				}
-			} else {
-				if (currentTrend == "up") {
-					if (swing.isTop()) {
-						if (highestSwingHigh == null || (swing.getValue() > highestSwingHigh.getValue())) {
-							highestSwingHigh = swing;
-							leadingSwingLow = lastSwingLow;
-						}
-					} else {
-						if (leadingSwingLow != null) {
-							if (swing.getValue() < leadingSwingLow.getValue()) {
-								if (ltfTrend) {
-									if (highestSwingHigh != null) {
-										this.swingManager.swingsTTFKeys.add(highestSwingHigh.getIndex());
-									}
-								}
-								
-								lowestSwingLow = swing;
-								leadingSwingLow = null;
-								leadingSwingHigh = lastSwingHigh;
-								
-								currentTrend = "down";
-								if (!ltfTrend) {
-									onWave2 = true;
-									wave2Index = swing.getIndex();
-									reachedZone = false;
-									invalidatedZone = false;
-									//debug(String.format("Wave 2 confirmed on index #%d", wave2Index));
-								}
-							}
-						}
-					}
-				} else if (currentTrend == "down") {
-					if (swing.isBottom()) {
-						if (lowestSwingLow == null || (swing.getValue() < lowestSwingLow.getValue())) {
-							lowestSwingLow = swing;
-							leadingSwingHigh = lastSwingHigh;
-						}
-					} else {
-						if (leadingSwingHigh != null) {
-							if (swing.getValue() > leadingSwingHigh.getValue()) {
-								if (ltfTrend) {
-									if (lowestSwingLow != null) {
-										this.swingManager.swingsTTFKeys.add(lowestSwingLow.getIndex());
-									}
-								}
-								
-								highestSwingHigh = swing;
-								leadingSwingHigh = null;
-								leadingSwingLow = lastSwingLow;
-								
-								currentTrend = "up";
-								if (!ltfTrend) {
-									onWave2 = true;
-									wave2Index = swing.getIndex();
-									reachedZone = false;
-									invalidatedZone = false;
-									//debug(String.format("Wave 2 confirmed on index #%d", wave2Index));
-								}
-							}
-						}
-					}
-				}
-			}
-			
-			if (swing.isTop())
-				lastSwingHigh = swing;
-			else
-				lastSwingLow = swing;
-		}
-	}
-	
-	public void computeRetraction(DataSeries series) {
-		validRetraction = false;
-		
-		//debug(String.format("On Wave 2? %b", onWave2));
-		if (!onWave2) return;
-		
-		if (this.swingManager.swingsTTFKeys.size() < 2) {
-			debug("Not enough swing points to compute correction");
-			return;
-		}
-		
-		int swing1Index = this.swingManager.swingsTTFKeys.get(this.swingManager.swingsTTFKeys.size() - 2);
-		int swing2Index = this.swingManager.swingsTTFKeys.get(this.swingManager.swingsTTFKeys.size() - 1);
-		
-		SwingPoint swing1 = null;
-		SwingPoint swing2 = null;
-		
-		for (SwingPoint swing : this.swingManager.swingsLTF) {
-			if (swing.getIndex() == swing1Index) swing1 = swing;
-			if (swing.getIndex() == swing2Index) swing2 = swing;
-			
-			if (swing1 != null & swing2 != null) break;
-		}
-		
-		if (swing1 == null || swing2 == null) {
-			debug("Not enough swing points to compute correction");
-			return;
-		}
-		
-		validRetraction = true;
-		
-		if (swing2.isTop()) {
-			double diff = swing2.getValue() - swing1.getValue();
-			
-			retraction50 = diff * 0.5f + swing1.getValue();
-			retraction618 = diff * 0.618f + swing1.getValue();
-			retraction = (swing2.getValue() - series.getClose()) * 100.0f / diff;
-		} else {
-			double diff = swing1.getValue() - swing2.getValue();
-			
-			retraction50 = diff * 0.5f + swing2.getValue();
-			retraction618 = diff * 0.618f + swing2.getValue();
-			retraction = (series.getClose() - swing2.getValue()) * 100.0f / diff;
-		}
-		
-		/*debug(String.format("Swing 2 is Top? %b", swing2.isTop()));
-		debug(String.format("Close: %.5f", series.getClose()));
-		debug(String.format("Swing 1: %.5f", swing1.getValue()));
-		debug(String.format("Swing 2: %.5f", swing2.getValue()));
-		debug(String.format("Retraction: %.2f%%", retraction));
-		debug(String.format("Retraction 50%%: %.5f", retraction50));
-		debug(String.format("Retraction 61.8%%: %.5f", retraction618));*/
-	}
-	
 	public void prepareTrade(DataContext ctx, DataSeries series) {
-		if (!onWave2) return;
-		if (!validRetraction) return;
+		if (!this.trendManager.onWave2) return;
+		if (!this.trendManager.validRetraction) return;
 		
-		if (retraction > 40.0f) reachedZone = true;
-		if (retraction > 66.8f) invalidatedZone = true;
+		if (this.trendManager.retraction > 40.0f) this.trendManager.reachedZone = true;
+		if (this.trendManager.retraction > 66.8f) this.trendManager.invalidatedZone = true;
 		
-		if (reachedZone && !invalidatedZone) {
+		if (this.trendManager.reachedZone && !this.trendManager.invalidatedZone) {
 			debug("Trade is valid. We can create the Stop order now.");
 			
 			SwingPoint lastSwingHigh = null;
@@ -305,7 +148,7 @@ public class FibonacciStrategy extends Study {
 				if (swing.isBottom()) lastSwingLow = swing;
 			}
 			
-			if (currentTrend == "up") {
+			if (this.trendManager.currentTrend == "up") {
 				if (lastSwingHigh != null) {
 					debug(String.format("BUY @ %.5f", lastSwingHigh.getValue()));
 					stopPrice = (float)lastSwingHigh.getValue();
@@ -314,7 +157,7 @@ public class FibonacciStrategy extends Study {
 					orderAction = Enums.OrderAction.BUY;
 					//ctx.signal(lastSwingLow.getIndex(), Signals.BUY_STOP, "BUY", series.getClose(lastSwingLow.getIndex()));
 				}
-			} else if (currentTrend == "down") {
+			} else if (this.trendManager.currentTrend == "down") {
 				if (lastSwingLow != null) {	
 					debug(String.format("SELL @ %.5f", lastSwingLow.getValue()));
 					stopPrice = (float)lastSwingLow.getValue();
@@ -373,13 +216,10 @@ public class FibonacciStrategy extends Study {
 		DataSeries series = ctx.getDataSeries();
 		
 		this.swingManager.update(series);
-		
-		computeTrend(true);
-		computeTrend(false);
+		this.trendManager.update(this.swingManager.swingsLTF, this.swingManager.swingsTTFKeys, series);
 		
 		drawMarkersAndLines();
 		
-		computeRetraction(series);
 		prepareTrade(ctx, series);
 
 		super.onBarClose(ctx);
@@ -396,7 +236,7 @@ public class FibonacciStrategy extends Study {
 			}
 		}
 		
-		if (invalidatedZone && currentOrder != null && ctx.getPosition() == 0) {
+		if (this.trendManager.invalidatedZone && currentOrder != null && ctx.getPosition() == 0) {
 			//ctx.cancelOrders();
 		}
 		
