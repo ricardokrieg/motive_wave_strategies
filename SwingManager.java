89a6829649aca9a91a501356335f8879563224e7
Ricardo Franco <ricardo.krieg@gmail.com>
2022-08-09 13:05:51 +0000

    SwingManager.java edited online with Bitbucket

--- SwingManager.java
+++ SwingManager.java
@@ -11,6 +11,7 @@ import java.util.TreeSet;
 import com.motivewave.platform.sdk.common.DataSeries;
 import com.motivewave.platform.sdk.common.SwingPoint;
 
+
 public class SwingManager {
 	int strength;
 	
