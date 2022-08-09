0f11c2d62ed8ee7886d2dd258ddcb7923b58f62c
Ricardo Franco <ricardo.krieg@gmail.com>
2022-08-09 02:52:26 +0000

    OrderObject.java created online with Bitbucket

---
+++ OrderObject.java
@@ -0,0 +1,41 @@
+package ricardo_franco;
+
+import com.motivewave.platform.sdk.common.Enums.OrderAction;
+
+public class OrderObject {
+	
+	OrderAction orderAction;
+	float price;
+	boolean running;
+
+	public OrderObject(OrderAction orderAction, float price) {
+		this.orderAction = orderAction;
+		this.price = price;
+		
+		this.running = false;
+	}
+	
+	public void execute() {
+		this.running = !this.running;
+	}
+	
+	public boolean isEntryPrice(float price) {
+		return (this.isBuy() && price >= this.price) || (this.isSell() && price <= this.price); 
+	}
+	
+	public boolean isStopLossPrice(float price) {
+		return false;
+	}
+	
+	public boolean isTakeProfitPrice(float price) {
+		return false;
+	}
+	
+	public boolean isBuy() {
+		return this.orderAction == OrderAction.BUY;
+	}
+	
+	public boolean isSell() {
+		return this.orderAction == OrderAction.SELL;
+	}
+}
